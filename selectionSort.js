0e5d346a6e8a8107d87417a9cb2d0cfea4992bce
Nick Fredman <nick@Nicks-MacBook-Pro.local>
2018-11-16 13:27:14 -0600

    selection sort working

--- selectionSort.js
+++ selectionSort.js
@@ -1,1 +1,27 @@
-function selectionSort(arr) {}
+function selectionSort(arr) {
+  const arrCopy = arr.slice();
+  const sortedArr = [];
+
+  function sortLargestItem(arr) {
+    let largest = arrCopy[0];
+    let largestIndex = 0;
+    for (let i = 1; i < arrCopy.length; i++) {
+      if (arrCopy[i] > largest) {
+        largest = arrCopy[i];
+        largestIndex = i;
+      }
+    }
+    return largestIndex;
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    let currentLargest = sortLargestItem(arrCopy);
+    sortedArr.push(arr[currentLargest]);
+    arrCopy.splice(currentLargest, 1);
+  }
+
+  return sortedArr;
+}
+
+let sorted = selectionSort([0, 4, 3, 2, 5]);
+sorted;
